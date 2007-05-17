dd8cbbd42107ec99a53b806b504e5bd81f1b01dc
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-17 23:34:37 +0000

    Stubbin Stubbin Stubbin
    git-svn-id: http://activereload.svn.engineyard.com/xorn/trunk@1441 8456e5f2-bb15-0410-bd20-b44782a1fb1e

---
+++ config/initializers/general.rb
@@ -0,0 +1,3 @@
+if RAILS_ENV == 'development'
+  ENV["RAILS_ASSET_ID"] = ''
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.resources :products, :has_many => [ :comments, :sales ], :has_ony => :seller
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "dashboard"
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
