f4292c312ca5e2464c4f0a916c5fd7f71f3fa5d1
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-18 03:26:01 +0000

    add svn/repos to Repository model
    git-svn-id: http://activereload.svn.engineyard.com/xorn/trunk@1451 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,4 @@
+require 'svn/repos'
 class Repository < ActiveRecord::Base
   has_permalink :name
   validates_presence_of :name, :path, :permalink
@@ -6,4 +7,13 @@ class Repository < ActiveRecord::Base
   def path=(value)
     write_attribute(:path, value ? value.to_s.chomp('/') : nil)
   end
+  
+  def latest_revision
+    @latest_revision ||= backend.youngest_rev
+  end
+  
+  protected
+    def backend
+      @backend ||= Svn::Repos.open(path)
+    end
 end
