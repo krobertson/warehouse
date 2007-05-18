ba936062ffe6b8e43f9b22e1c95e33fadb6a62f1
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-18 05:26:22 +0000

    allow #url_for_node to take a string path like foo/bar
    git-svn-id: http://activereload.svn.engineyard.com/xorn/trunk@1458 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/helpers/browser_helper.rb
+++ app/helpers/browser_helper.rb
@@ -1,5 +1,6 @@
 module BrowserHelper
   def url_for_node(node)
-    @revision ? rev_browser_path(:paths => node.paths, :rev => params[:rev]) : browser_path(:paths => node.paths)
+    paths = node.respond_to?(:paths) ? node.paths : node.to_s.split('/')
+    @revision ? rev_browser_path(:paths => paths, :rev => params[:rev]) : browser_path(:paths => paths)
   end
 end
