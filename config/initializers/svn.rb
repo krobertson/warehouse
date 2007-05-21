a900861a8f33dc41a335016b4f35037ecfc31aca
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-21 02:26:45 +0000

    add svn initializer
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1508 8456e5f2-bb15-0410-bd20-b44782a1fb1e

---
+++ config/initializers/svn.rb
@@ -0,0 +1,28 @@
+require 'svn/core'
+require 'svn/repos'
+require 'svn/delta'
+class Svn::Delta::ChangedEditor
+  def add_file_with_collection(*args)
+    @dirty_run_count = @dirty_run_count ? @dirty_run_count + 1 : 0
+    GC.start if (@dirty_run_count % 20).zero?
+    add_file_without_collection(*args)
+  end
+  
+  alias_method_chain :add_file, :collection
+end
+
+module Svn
+  module Fs
+    class FileSystem
+      @@gc_count = 0
+
+      def root_with_gc(rev=nil)
+        @@gc_count = (@@gc_count + 1) % 20
+        GC.start if @@gc_count.zero?
+        root_without_gc(rev)
+      end      
+      alias_method :root_without_gc, :root      
+      alias_method :root, :root_with_gc
+    end
+  end
+end
