996ef01f037ae5a3d216ff3faca17c0425152a6a
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-22 23:47:29 +0000

    About to try the new code highlighter
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1531 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- public/javascripts/code/html.js
+++ public/javascripts/code/html.js
@@ -2,17 +2,21 @@ CodeHighlighter.addStyle('html', {
 	comment : {
 		exp: /&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/
 	},
-	tag : {
-		exp: /(&lt;\/?)([a-zA-Z]+\s?)/, 
-		replacement: "$1<span class=\"$0\">$2</span>"
-	},
-	string : {
-		exp  : /'[^']*'|"[^"]*"/
-	},
-	attribute : {
-		exp: /\b([a-zA-Z-:]+)(=)/, 
-		replacement: "<span class=\"$0\">$1</span>$2"
-	},
+  // tag : {
+  //  exp: /(&lt;\/?)([a-zA-Z1-6]+\s?)/, 
+  //  replacement: "$1<span class=\"$0\">$2</span>"
+  // },
+  // string : {
+  //  exp  : /'[^']*'|"[^"]*"/
+  // },
+  // attribute : {
+  //  exp: /\b([a-zA-Z-:]+)(=)/, 
+  //  replacement: "<span class=\"$0\">$1</span>$2"
+  // },
+  tag : {
+    exp: /&lt;("[^"]*"|'[^']*'|[^'">])*&gt;/,
+    replacement: "<span class=\"$0\">$0</span>"
+  },
 	doctype : {
 		exp: /&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/
 	}
