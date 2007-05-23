d83b37b7788734bb306937e708bee5ff657cd13f
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-23 00:16:48 +0000

    Fix what I fucked up with code highlighting
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1534 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- public/javascripts/code/html.js
+++ public/javascripts/code/html.js
@@ -2,20 +2,16 @@ CodeHighlighter.addStyle('html', {
 	comment : {
 		exp: /&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/
 	},
-  // tag : {
-  //  exp: /(&lt;\/?)([a-zA-Z1-6]+\s?)/, 
-  //  replacement: "$1<span class=\"$0\">$2</span>"
-  // },
-  // string : {
-  //  exp  : /'[^']*'|"[^"]*"/
-  // },
-  // attribute : {
-  //  exp: /\b([a-zA-Z-:]+)(=)/, 
-  //  replacement: "<span class=\"$0\">$1</span>$2"
-  // },
   tag : {
-    exp: /&lt;("[^"]*"|'[^']*'|[^'">])*&gt;/,
-    replacement: "<span class=\"$0\">$0</span>"
+   exp: /(&lt;\/?)([a-zA-Z1-6]+\s?)/, 
+   replacement: "$1<span class=\"$0\">$2</span>"
+  },
+  string : {
+   exp  : /'[^']*'|"[^"]*"/
+  },
+  attribute : {
+   exp: /\b([a-zA-Z-:]+)(=)/, 
+   replacement: "<span class=\"$0\">$1</span>$2"
   },
 	doctype : {
 		exp: /&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/
