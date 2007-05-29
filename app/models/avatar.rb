c35b10445d94e567ff95524fc8229b6a60e3ac2a
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-05-29 02:54:21 +0000

    show avatars in changeset
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1563 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,5 +1,5 @@
 class Avatar < ActiveRecord::Base
-  has_attachment :storage => :file_system, :content_type => :image, :resize_to => '40x40'
+  has_attachment :storage => :file_system, :content_type => :image, :resize_to => '32x32'
   validates_as_attachment
   validates_length_of :filename, :maximum => 255
   before_destroy { |r| User.update_all 'avatar_id = NULL', ['avatar_id = ?', r.id] unless r.new_record? }
