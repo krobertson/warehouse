080305d19a8e2d1087f1fa310262706b641c649e
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-18 20:24:26 +0000

    simplify warehouse config file
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1641 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- lib/warehouse.rb
+++ lib/warehouse.rb
@@ -2,11 +2,4 @@ require 'digest/sha1'
 module Warehouse
   @@forum_url = "http://forum.activereload.net/licenses/%s/installs"
   mattr_accessor :multiple_repositories, :domain, :forum_url
-
-  def self.configure(&block)
-    require 'dispatcher'
-    Dispatcher.to_prepare do
-      block.call(self)
-    end
-  end
 end
