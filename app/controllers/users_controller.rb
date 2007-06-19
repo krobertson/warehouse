ad732226aa170fba82c89c59e7aab03e0e99798c
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-19 00:27:27 +0000

    fix profile editing
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1649 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   before_filter :login_required
   
   def update
-    if current_user.update_attributes(params[:user])
-      redirect_to profile_path
-    else
-      render :action => 'show'
+    respond_to do |format|
+      if current_user.update_attributes(params[:user])
+        format.html { redirect_to profile_path }
+        format.js
+      else
+        format.html { render :action => 'show' }
+        format.js
+      end
     end
   end
 end
