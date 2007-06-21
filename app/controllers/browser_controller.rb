78f73671b275821bd3db11a7e1b9dd39c78319de
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-21 03:18:49 +0000

    Moving new repo to sheets
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1688 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -21,9 +21,6 @@ class BrowserController < ApplicationController
   end
 
   protected
-    def access_denied(options = {})
-      @error = options[:error] || "A login is required to visit this page."
-    end
 
     def find_node
       @revision = params[:rev][1..-1].to_i if params[:rev]
