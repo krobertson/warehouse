c33e77cd054b2aba689df5eb79eb80012967007d
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-22 02:11:34 +0000

    Moving permission edit into sheets
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1704 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -145,14 +145,3 @@ Event.addBehavior({
     Permissions.remove(this.up());
   }
 });
-
-if(window.google) {
-  Event.onReady(function() {
-    var rules = $A(document.styleSheets[0].cssRules);
-    rules.each(function(rule) {
-      console.log(rule.selectorText, $$(rule.selectorText).size());
-    });
-  });
-} else {
-  alert('no gears');
-}
