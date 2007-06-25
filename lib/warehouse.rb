d28f0b62fe963baed10cf5f7b90961e94c2c46de
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-25 19:41:19 +0000

    add warehouse version
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1746 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- lib/warehouse.rb
+++ lib/warehouse.rb
@@ -1,6 +1,31 @@
 module Warehouse
+  class Version
+    attr_reader :major, :minor, :tiny
+    
+    def initialize(major, minor, tiny)
+      @major = major
+      @minor = minor
+      @tiny  = tiny
+    end
+    
+    def ==(version)
+      if version.is_a? Version
+        version.major == major && version.minor == minor && version.tiny == tiny
+      else
+        version.to_s == to_s
+      end
+    end
+    
+    def to_s
+      @string ||= [@major, @minor, @tiny] * '.'
+    end
+    
+    alias_method :inspect, :to_s
+  end
+
   class << self
-    attr_accessor :domain, :forum_url, :permission_command, :password_command, :mail_from
+    attr_accessor :domain, :forum_url, :permission_command, :password_command, :mail_from, :version
   end
   self.forum_url = "http://forum.activereload.net/licenses/%s/installs"
+  self.version   = Version.new(0, 9, 0)
 end
