b47584f1e848b87924ecf99d065237f53b6ad420
justin <justin@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-06-25 00:51:05 +0000

    Add new site for Tuesday and change member stuff around as discussed
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1737 8456e5f2-bb15-0410-bd20-b44782a1fb1e

---
+++ site/newsite/javascripts/lowpro-lite.js
@@ -0,0 +1,106 @@
+// LowPro by Dan Webb (http://danwebb.net)
+// I just ripped out the parts I don't need with the newer version of Protoype, feel free to 
+// steal this file for your own use.  
+// Extends Event to add onReady and addBehavior 
+//
+Object.extend(Event, {
+  _domReady : function() {
+    if (arguments.callee.done) return;
+    arguments.callee.done = true;
+
+    if (Event._timer)  clearInterval(Event._timer);
+    
+    Event._readyCallbacks.each(function(f) { f() });
+    Event._readyCallbacks = null;
+    
+  },
+  onReady : function(f) {
+    if (!this._readyCallbacks) {
+      var domReady = this._domReady;
+      
+      if (domReady.done) return f();
+      
+      if (document.addEventListener)
+        document.addEventListener("DOMContentLoaded", domReady, false);
+        
+        /*@cc_on @*/
+        /*@if (@_win32)
+            var dummy = location.protocol == "https:" ?  "https://javascript:void(0)" : "javascript:void(0)";
+            document.write("<script id=__ie_onload defer src='" + dummy + "'><\/script>");
+            document.getElementById("__ie_onload").onreadystatechange = function() {
+                if (this.readyState == "complete") { domReady(); }
+            };
+        /*@end @*/
+        
+        if (/WebKit/i.test(navigator.userAgent)) { 
+          this._timer = setInterval(function() {
+            if (/loaded|complete/.test(document.readyState)) domReady(); 
+          }, 10);
+        }
+        
+        Event.observe(window, 'load', domReady);
+        Event._readyCallbacks =  [];
+    }
+    Event._readyCallbacks.push(f);
+  }
+});
+
+Event.addBehavior = function(rules) {
+  var ab = this.addBehavior;
+  Object.extend(ab.rules, rules);
+  
+  if (!ab.responderApplied) {
+    Ajax.Responders.register({
+      onComplete : function() { 
+        if (Event.addBehavior.reassignAfterAjax) 
+          setTimeout(function() { ab.unload(); ab.load(ab.rules) }, 10);
+      }
+    });
+    ab.responderApplied = true;
+  }
+  
+  if (ab.autoTrigger) {
+    this.onReady(ab.load.bind(ab, rules));
+  }
+  
+};
+
+Object.extend(Event.addBehavior, {
+  rules : {}, cache : [],
+  reassignAfterAjax : true,
+  autoTrigger : true,
+  
+  load : function(rules) {
+    for (var selector in rules) {
+      var observer = rules[selector];
+      var sels = selector.split(',');
+      sels.each(function(sel) {
+        var parts = sel.split(/:(?=[a-z]+$)/), css = parts[0], event = parts[1];
+        $$(css).each(function(element) {
+          if (event) {
+            $(element).observe(event, observer);
+            Event.addBehavior.cache.push([element, event, observer]);
+          } else {
+            if (!element.$$assigned || !element.$$assigned.include(observer)) {
+              if (observer.attach) observer.attach(element);
+              
+              else observer.call($(element));
+              element.$$assigned = element.$$assigned || [];
+              element.$$assigned.push(observer);
+            }
+          }
+        });
+      });
+    }
+  },
+  
+  unload : function() {
+    this.cache.each(function(c) {
+      Event.stopObserving.apply(Event, c);
+    });
+    this.cache = [];
+  }
+  
+});
+
+Event.observe(window, 'unload', Event.addBehavior.unload.bind(Event.addBehavior));
