3c9245751188c4409d36907bba628946326ddc41
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-07-04 06:10:38 +0000

    dont validate uniqueness of user login/email if they're blank
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1855 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,10 @@ class User < ActiveRecord::Base
   attr_accessor :password
   validates_format_of       :email, :with => /(\A(\s*)\Z)|(\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z)/i, :allow_nil => true
   validates_confirmation_of :password, :allow_nil => true
-  validates_uniqueness_of   :identity_url, :email, :login, :allow_nil => true
+  validates_uniqueness_of :identity_url, :allow_nil => true
+  [:email, :login].each do |attr|
+    validates_uniqueness_of attr, :if => lambda { |u| !u.send(attr).blank? }
+  end
   validate :presence_of_identity_url_or_email
   before_create :set_default_attributes
   before_save   :sanitize_email
