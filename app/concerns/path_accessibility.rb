d11c99324187ac8e2e9dbe41e45aa6110b8b55f0
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-07-06 06:12:02 +0000

    slight fix for path_accessibility
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@1891 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/concerns/path_accessibility.rb
+++ app/concerns/path_accessibility.rb
@@ -1,6 +1,6 @@
 module PathAccessibility
   def accessible_by?(user)
-    return true  if (user && user.admin?) || changeset.repository.public?
+    return true  if (user && user.admin?) || repository.public?
     return false if user.nil?
     paths = user.permissions.paths_for(repository)
     paths == :all || paths.any? { |p| path == "#{p}" || path =~ %r{^#{p}/} }
