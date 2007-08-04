6fb2f7a4a65ec08a48822cf6085652ace36dac8d
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-08-04 14:55:05 +0000

    add xml simple fix
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@2001 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- config/initializers/general.rb
+++ config/initializers/general.rb
@@ -3,4 +3,5 @@ require 'warehouse'
 if RAILS_ENV == 'development'
   ENV["RAILS_ASSET_ID"] = ''
 end
+REXML::Document.class_eval { def doctype() nil end }
 ActionContentFilter.preserved_instance_variables = %w(@title @onready @fullscreen @current_sheets @content_for_scripts @content_for_onready @content_for_javascript @content_for_sidebar)
