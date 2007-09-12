e55d6261a502911ab2b01d1fb94d6119d299c71d
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-09-12 22:04:00 +0000

    add plugin saving
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@2147 8456e5f2-bb15-0410-bd20-b44782a1fb1e

--- app/controllers/plugins_controller.rb
+++ app/controllers/plugins_controller.rb
@@ -1,31 +1,18 @@
 class PluginsController < ApplicationController
   skip_before_filter :check_for_repository
   before_filter :admin_required
-  before_filter(:only => :index) { Warehouse::Plugins.load }
 
   def index
+    Warehouse::Plugins.load
     @plugin  = Plugin.new
     @plugins = Warehouse::Plugins.discovered
   end
   
-  def create
-    @plugin = Plugin.create(params[:plugin])
-    respond_to do |format|
-      format.js
-    end
-  end
-  
   def update
     @plugin = Plugin.find(params[:id])
+    params[:plugin] ||= {}
+    @plugin.active  = params[:plugin].delete(:active)
     @plugin.options = params[:plugin].to_hash
-    respond_to do |format|
-      format.js
-    end
-  end
-  
-  def destroy
-    @plugin = Plugin.find(params[:id])
-    @plugin.destroy
     respond_to do |format|
       format.js
     end
--- test/functional/plugins_controller_test.rb
+++ test/functional/plugins_controller_test.rb
@@ -4,15 +4,29 @@ require 'plugins_controller'
 # Re-raise errors caught by the controller.
 class PluginsController; def rescue_action(e) raise e end; end
 
-class PluginsControllerTest < Test::Unit::TestCase
-  def setup
+context "Plugins Controller" do
+  fixtures :plugins
+  setup do
     @controller = PluginsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    login_as :rick
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  specify "should edit plugin attributes" do
+    put :update, :id => 2, :plugin => {'foo' => 'bar'}
+    plugins(:inactive).options.should == {'foo' => 'bar'}
+  end
+
+  specify "should activate plugin" do
+    plugins(:inactive).should.not.be.active
+    put :update, :id => 2, :plugin => {:active => '1'}
+    plugins(:inactive).reload.should.be.active
+  end
+
+  specify "should deactivate plugin" do
+    plugins(:foo).should.be.active
+    put :update, :id => 1, :plugin => {:active => '0'}
+    plugins(:foo).reload.should.not.be.active
   end
 end
