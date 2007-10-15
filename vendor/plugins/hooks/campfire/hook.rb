dbbd1f41891afb0eb034392ec133c2f8637df8b7
rick <rick@8456e5f2-bb15-0410-bd20-b44782a1fb1e>
2007-10-15 02:33:02 +0000

    add campfire
    git-svn-id: http://activereload.svn.engineyard.com/warehouse/trunk@2235 8456e5f2-bb15-0410-bd20-b44782a1fb1e

---
+++ vendor/plugins/hooks/campfire/hook.rb
@@ -0,0 +1,45 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), 'vendor/tinder/lib')
+
+Warehouse::Hooks.define :campfire do
+  title "Campfire"
+  author "activereload"
+  version "1.0"
+  homepage 'http://activereload.net'
+  notes <<-END_NOTES
+    This plugin logs into and speaks in a campfire room for every commit.
+    
+    This small hook requires a Campfire account, uses Tinder to talk to Campfire, and was adapted
+    from code written by Chris Wanstrath.
+    
+    http://campfirenow.com
+    http://tinder.rubyforge.org/
+    http://errtheblog.com
+  END_NOTES
+  
+  option :campfire, "Your campfire subdomain"
+  option :room, "(optional) Your Campfire room name."
+  option :user, "Your user name.  Can be an anchor if you want a link."
+  option :password, "Your campfire password"
+  option :url, "A string format for a direct link to a changeset.  ex: 'http://%s.wh.yourdomain.com/changesets/%s'"
+  
+  permalink do
+    options[:url] % [commit.repo[:subdomain], commit.revision.to_s]
+  end
+  
+  init do
+    require 'tinder'
+  end
+  
+  run do
+    campfire = Tinder::Campfire.new options[:campfire]
+    campfire.login options[:user], options[:password]
+    room = \
+      if options[:room]
+        campfire.rooms.select { |r| r.name == options[:room] } || campfire.rooms.first
+      else
+        campfire.rooms.first
+      end
+    
+    room.speak "#{repo[:subdomain]}: #{commit.author} committed [#{commit.revision}] #{permalink}"
+  end
+end
