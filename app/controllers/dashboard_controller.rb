d7a9db5aee5425653cde39ce951960b8ec34efc6
Ken Robertson <ken@invalidlogic.com>
2008-10-04 15:59:00 -0700

    Updated dashboard to display available repositories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,21 @@ class DashboardController < ApplicationController
   skip_before_filter :check_for_repository
 
   def index
-    if installed?
-      redirect_to(repository_subdomain.blank? ? send("root_#{logged_in? ? :changesets : :public_changesets}_path") : hosted_url(:browser))
-    else
+    unless installed?
       install
+      return
     end
+
+    unless logged_in?
+      @repositories = Repository.find_all_by_public(true)
+      if @repositories.length == 0
+        redirect_to '/sessions/new'
+        return
+      end
+    end
+
+    @repositories ||= admin? ? Repository.find(:all) : current_user.administered_repositories
+    render :layout => 'application'
   end
+
 end
